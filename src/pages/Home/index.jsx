62b13ebff6ba4687590d5bbb3b64abcb66f6dfe7
Darlan Mendes <dedsondarlan@gmail.com>
2023-11-20 16:23:02 -0300

    Ajuste da tela

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,7 @@
 import tenis from "../../assets/tenis.webp"
+import {Link} from "react-router-dom"
+import imgDesk from "../../assets/promo-banner-desk.png"
+import imgMob from "../../assets/promo-banner-mobile.png"
 function Card({ highlighted }) {
     return (
         <div className=" shadow-sm w-36 h-52 md:w-44 md:h-64 rounded-md bg-white">
@@ -13,7 +16,18 @@ function Card({ highlighted }) {
         </div>
     )
 }
+function Promo() {
+    const promo = {imgMob:imgMob, imgDesk:imgDesk}
+    console.log(promo.imgDesk)
+    return (
+        <div className="w-screen relative md:block flex justify-center ">
+            <img src={promo.imgMob} alt="Banner Promo mobile"  className="md:hidden w-screen max-h-[400px] object-cover"/>
+            <img src={promo.imgDesk} alt="Banner Promo desktop " className="hidden md:flex"/>
+            <Link to="/promo" className="bg-orange-500 absolute bottom-10 md:right-10 lg:right-30 xl:right-80 w-[331px] md:w-80 text-center h-16 rounded-md flex items-center justify-center">Aproveitar Oferta</Link>
+        </div>
 
+    )
+}
 function Destaques() {
     const highlighted = {
         tittle: "Nique Air Surf",
@@ -25,12 +39,12 @@ function Destaques() {
     return (
         <div className="bg-zinc-50  w-screen flex flex-col xl:px-24 ">
 
-            <h1 className="text-blue-900 font-semibold text-xl py-6 ml-5 max-w-5xl w-screen">Destaques</h1>
-            <div className="grid grid-cols-2 xl:grid-cols-5 md:grid-cols-3 justify-around justify-items-center items-center gap-y-10 lg:grid-cols-4 2xl:grid-cols-6 max-w-5xl">
+            <h1 className="text-blue-900 font-semibold text-xl py-6 pl-7 max-w-5xl w-screen">Destaques</h1>
+            <div className="grid grid-cols-2  md:grid-cols-3 justify-around justify-items-center items-center gap-y-10 lg:grid-cols-5 xl:grid-cols-6 max-w-5xl">
                 {highlighteds.map((highlighted, index) => (
-                   
-                        <Card key={index} highlighted={highlighted} />
-                    
+
+                    <Card key={index} highlighted={highlighted} />
+
                 ))}
             </div>
         </div>
@@ -39,7 +53,8 @@ function Destaques() {
 export default function Home() {
     return (
         <main>
-            <section className="flex flex-wrap py-10">
+            <section className="flex flex-wrap md:pt-10">
+                <Promo />
                 <Destaques />
             </section>
         </main>
