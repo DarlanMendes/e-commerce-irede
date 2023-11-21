cd6c39d4d2b3d52fcf8e6ad73251fe339dff2d9d
Darlan Mendes <dedsondarlan@gmail.com>
2023-11-21 16:10:12 -0300

    Ajuste tela login e finalização tela cadastre-se

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import imgLogo from "../../assets/logo-e-rede.png"
 export default function SignIn() {
+    const navigate= useNavigate()
     return (
         <main className="flex flex-col  gap-7 pt-8 pb-7 lg:p-0 lg:flex-row-reverse">
             <section className="lg:w-[50vw] flex flex-col justify-center items-center gap-7">
                 <h1 className="hidden lg:block text-3xl max-w-[381px] font-light">Sua nova experiência em compras online</h1>
-                <img src={imgLogo} alt="" className="lg:h-[150px] lg:w-[383px] object-cover"/>
+                <img src={imgLogo} alt="" className="lg:h-[150px] lg:w-[383px] object-cover"
+                onClick={()=>navigate("/")}
+                />
             </section>
             <section className="lg:w-[50vw] lg:bg-slate-100 h-screen flex flex-col lg:justify-center items-center">
             <form className="flex flex-col justify-center items-center bg-white px-5 py-[14px] max-w-[450px] w-full">
@@ -13,11 +16,11 @@ export default function SignIn() {
                 <div className="flex flex-col gap-5 w-full items-center justify-center">
                     <label className="text-black flex flex-col w-full justify-center items-center">
                         <h1 className="pb-2 pl-2 w-full max-w-[280px]">Email*</h1>
-                        <input placeholder="Digite seu e-mail" className=" bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
+                        <input placeholder="Digite seu e-mail" required type="email" className=" bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
                     </label>
                     <label className="text-black flex flex-col w-full justify-center items-center">
                         <h1 className="pb-2 pl-2 w-full max-w-[280px]">Senha*</h1>
-                        <input placeholder="Digite sua senha" className="bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
+                        <input placeholder="Digite sua senha" required type="password" className="bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
                     </label>
 
                 </div>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,43 @@
+import { Link,useNavigate } from "react-router-dom";
+import imgLogo from "../../assets/logo-e-rede.png"
 export default function SignUp(){
+    const navigate=useNavigate()
     return(
-        <main>
-            Cadastro
-        </main>
+        <main className="flex flex-col  gap-7 pt-8 pb-7 lg:p-0 lg:flex-row-reverse">
+        <section className="lg:w-[50vw] flex flex-col justify-center items-center gap-7">
+            <h1 className="hidden lg:block text-3xl max-w-[381px] font-light">Sua nova experiência em compras online</h1>
+            <img src={imgLogo} alt="" className="lg:h-[150px] lg:w-[383px] object-cover"
+             onClick={()=>navigate("/")}
+            />
+        </section>
+        <section className="lg:w-[50vw] lg:bg-slate-100 h-screen flex flex-col lg:justify-center items-center">
+        <form className="flex flex-col justify-center items-center bg-white px-5 py-[14px] max-w-[450px] w-full">
+            <h1 className="text-black text-2xl font-semibold pb-6">Cadastre-se</h1>
+            <div className="flex flex-col gap-5 w-full items-center justify-center">
+            <label className="text-black flex flex-col w-full justify-center items-center">
+                    <h1 className="pb-2 pl-2 w-full max-w-[280px]">Nome*</h1>
+                    <input placeholder="Digite seu nome" required className=" bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
+                </label>
+                <label className="text-black flex flex-col w-full justify-center items-center">
+                    <h1 className="pb-2 pl-2 w-full max-w-[280px]">Email*</h1>
+                    <input placeholder="Digite seu e-mail" required type="email" className=" bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
+                </label>
+                <label className="text-black flex flex-col w-full justify-center items-center">
+                    <h1 className="pb-2 pl-2 w-full max-w-[280px]">Senha*</h1>
+                    <input placeholder="Digite sua senha" required type="password" className="bg-slate-100 p-3 rounded-md min-w-[260px] outline-none lg:w-[300px]" />
+                </label>
+
+            </div>
+            <button className="w-full max-w-[300px] flex items-center justify-center bg-orange-500 h-[60px] rounded-lg font-bold mt-4 ">
+                Fazer Cadastro
+            </button>
+            <Link to="/sign-in" className="text-stone-500 py-[10px]">
+                Já possui cadastro?<span className="text-orange-500">Clique Aqui</span>
+            </Link>
+        </form>
+        </section>
+        
+
+    </main>
     )
 }
