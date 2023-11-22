a572090e5e9980c98c77adcbb26c2b6c809cca89
Darlan Mendes <dedsondarlan@gmail.com>
2023-11-22 17:12:28 -0300

    Correção bug carrinho

--- src/components/ModalCarrinho/index.jsx
+++ src/components/ModalCarrinho/index.jsx
@@ -8,10 +8,10 @@ export default function ModalCarrinho() {
     
     let listCart = []
     let total = 0
-    cart.products.forEach((product, index) => {
+    cart?.products.forEach((product, index) => {
 
-        let teste = products.filter(item => item.id === product.product_id)
-        listCart.push({ ...teste[0], qtt: cart.products[index].qtt })
+        let teste = products?.filter(item => item.id === product.product_id)
+        listCart.push({ ...teste[0], qtt: cart?.products[index].qtt })
     })
     listCart.forEach((item)=>{
         total += item.qtt * eval(item.price.replace(",","."))
@@ -21,7 +21,7 @@ export default function ModalCarrinho() {
     return (
         <div  className="bg-zinc-50 text-black w-72 p-7 rounded-lg absolute top-2 right-2 z-50 shadow">
             <h1 className="pb-4 font-bold border-b-[1px] border-stone-800">Meu Carrinho</h1>
-            {listCart.map((item) => (
+            {listCart.length>0?listCart.map((item) => (
                 <div key={item.id} className="flex gap-4 pr-4 pt-4">
                 
                     <img src={item.img} alt={item.tittle} className="w-20 h-[70px]" />
@@ -34,7 +34,11 @@ export default function ModalCarrinho() {
                 
                     
                 </div>
-            ))}
+            )):
+            <h1 className="p-4 ">
+                Você não possui itens adicionados ao carrinho
+                </h1>
+                }
             <h1 className="py-4 border-t-[1px] border-stone-900 mt-4 text-base font-semibold">
                 Valor total:
                 <strong className="text-blue-900 font-bold ml-4">R$ {total}</strong> 
